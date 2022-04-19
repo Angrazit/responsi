58e6e7a13ab9e951df5e9a4f75511daaea051245
angrazit@amikom.ac.id <angrazit@gmail.com>
2022-04-19 16:40:11 +0700

    preoject responsi

---
+++ Karyawan.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman4266
+{
+    internal class Karyawan
+    {
+        public string nim;
+        public string nama;
+        public int gajiBulanan;
+
+        public string Nim
+        {
+            get { return nim; }
+            set { nim = value; }
+        }
+        public string Nama
+        {
+            get { return nama; }
+            set { nama = value; }
+        }
+        public int GajiBulanan
+        {
+            get { return gajiBulanan; }
+            set { gajiBulanan = value; }
+        }
+
+        public Karyawan(string nim, string nama, int gajiBulanan)
+        {
+            this.nim = nim;
+            this.nama = nama;
+            this.gajiBulanan = gajiBulanan < 0 ? 0 : gajiBulanan;
+        }
+
+        public void tampilkanData()
+        {
+            Console.WriteLine("{0} {1}      {2}", nim, nama, gajiBulanan);
+        }
+
+        public void naikGaji()
+        {
+            int kenaikan;
+
+            kenaikan = gajiBulanan * 110;
+            gajiBulanan = kenaikan / 100;
+
+            
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman4266
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        
+            Karyawan anggota1 = new Karyawan("21.11.6624", "Zitha", 1500000);
+            Karyawan anggota2 = new Karyawan("21.11.4266", "Angga", 1000000);
+
+            Console.WriteLine("No Nim   / Nama      Gaji Bulanan");
+            Console.WriteLine("=================================");
+            Console.Write("");anggota1.tampilkanData();
+            Console.Write("");anggota2.tampilkanData();
+            Console.WriteLine();
+
+            anggota1.naikGaji();
+            anggota2.naikGaji();
+
+            Console.WriteLine("asyik naik gaji 10%");
+            Console.WriteLine();
+            Console.WriteLine("No Nim   / Nama      Gaji Bulanan");
+            Console.WriteLine("=================================");
+            Console.Write("");anggota1.tampilkanData();
+            Console.Write("");anggota2.tampilkanData();
+            Console.ReadKey();
+        }
+    }
+}
